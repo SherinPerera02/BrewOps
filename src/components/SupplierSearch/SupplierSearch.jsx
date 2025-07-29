6d7863de7293b885f4237c0dd32bb4494c236564
chiranjali <akachiranjali23@gmail.com>
2025-07-29 20:00:03 +0530

    compelete design

--- src/components/SupplierSearch/SupplierSearch.jsx
+++ src/components/SupplierSearch/SupplierSearch.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './SupplierSearch.css';
 
 export default function SupplierSearch() {
   const [searchInput, setSearchInput] = useState('');
@@ -13,19 +14,19 @@ export default function SupplierSearch() {
   };
 
   return (
-    <div className="supplier-search flex items-center gap-4 p-4">
+    <div className="searchbar">
       <input
         type="text"
         placeholder={`Search ${searchType === 'id' ? 'Supplier ID' : 'Supplier Name'}`}
         value={searchInput}
         onChange={handleInputChange}
-        className="mx-10 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300"
+        
       />
 
       <select
         value={searchType}
         onChange={handleTypeChange}
-        className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300"
+       
       >
         <option value="name">Supplier Name</option>
         <option value="id">Supplier ID</option>
--- src/pages/Supplier/SupplierHome/SupplierHome.jsx
+++ src/pages/Supplier/SupplierHome/SupplierHome.jsx
@@ -1,42 +1,72 @@
-import React from 'react'
-import NavigationBar from '../../../components/NavigationBar'
-import profile from '../../.../../../assets/profile.png'
-import './SupplierHomeStyle.css'
+import React from 'react';
+import NavigationBar from '../../../components/NavigationBar';
+import profile from '../../../assets/profile.png';
+import './SupplierHomeStyle.css';
 
-import { Link } from 'react-router-dom'
-import SupplierSearch from '../../../components/SupplierSearch'
-import Spinner from '../../../components/Spinner'
+import { Link } from 'react-router-dom';
+import SupplierSearch from '../../../components/SupplierSearch/SupplierSearch';
+import Footer from '../../../components/Footer';
 
 export default function SupplierHome() {
   return (
-    <div>
-      <NavigationBar/>
-      <nav>
-        <div className="container">
-          <div className='subnav'>
-            <Link to = "">Home</Link>
-            <Link to = "">Supplier</Link>
-            <Link to = "">Supplier Record</Link>
-            <Link to =""> <img src={profile}/> </Link>
-            
+    <div className="supplier-home-wrapper">
+      
+      <div className="nav">
+        <NavigationBar />
+        <nav>
+          <div className="container">
+            <div className="subnav">
+              <Link to="">Home</Link>
+              <Link to="">Supplier</Link>
+              <Link to="">Supplier Record</Link>
+              <Link to="">
+                <img src={profile} alt="Profile" />
+              </Link>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
 
+  
       <div className="body">
         <div className="body_container">
           <h1>Supplier List</h1>
           <div className="body_function">
             <button>Generate Report</button>
-            <Link to =''>Add New</Link>
+            <Link to="">Add New</Link>
           </div>
-          
+        </div>
+
+        <div className="searchbar-wrapper">
+          <SupplierSearch />
+        </div>
+
+        <div className="supplier_table">
+          <table>
+            <thead>
+              <tr>
+                <th>Supplier ID</th>
+                <th>Supplier Name</th>
+                <th>Contact Number</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Placeholder row */}
+              <tr>
+                <td colSpan="6" style={{ textAlign: 'center', padding: '20px' }}>
+                  No suppliers found.
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
 
-    <SupplierSearch />
-    <Spinner/>
       
+      <Footer />
     </div>
-  )
+  );
 }
